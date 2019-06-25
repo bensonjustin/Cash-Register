5adbf018ef9d47e6388374a23aa1646f0505027c
Justin <jbbenson12@gmail.com>
2019-06-25 13:06:45 -1000

    cash reg

--- js/calculator.js
+++ js/calculator.js
@@ -1,10 +1,17 @@
 let calculator = (function(){
 
+    let holdOp = '';
+    let lastValue = 0;
+    let currentValue = 0;
+
     let clickNum = document.querySelectorAll('.num');
     for (let i = 0; i<clickNum.length; i++){
     clickNum[i].addEventListener('click', dispNum);
     function dispNum(){
-        if (inputBox.textContent.length < 10 && inputBox.textContent !== '0'){
+        if (inputBox.textContent.length < 10 && inputBox.textContent !== '0' && lastValue != inputBox.textContent && currentValue != inputBox.textContent){
+            inputBox.textContent = inputBox.textContent + document.getElementsByClassName('num')[i].innerHTML;
+        }else if (lastValue == inputBox.textContent || currentValue == inputBox.textContent){
+            inputBox.textContent = '';
             inputBox.textContent = inputBox.textContent + document.getElementsByClassName('num')[i].innerHTML;
         }
     }
@@ -43,40 +50,34 @@ let calculator = (function(){
         }
     }
 
-    let holdOp = '';
-    let lastValue = 0;
-    let currentValue = 0;
-
     let clickAdd = document.querySelector('#add');
     clickAdd.addEventListener('click', dispAdd);
         
     function dispAdd(){
-        holdOp = 'add';
+        currentValue = inputBox.textContent;
         if (!lastValue){
             lastValue = Number(inputBox.textContent);
-            inputBox.textContent = '';
             console.log('firstadd')
-        }else{
+        }else if (lastValue && holdOp === 'add'){
             lastValue = Number(lastValue) + Number(inputBox.textContent);
-            inputBox.textContent = '';
             console.log(lastValue)
         }
+        holdOp = 'add';
     }
 
     let clickSubtract = document.querySelector('#subtract');
     clickSubtract.addEventListener('click', dispSubtract);
 
     function dispSubtract(){
-        holdOp = 'subtract';
+        currentValue = inputBox.textContent;
         if (!lastValue){
             lastValue = Number(inputBox.textContent)
-            inputBox.textContent = '';
             console.log('firstsubtract')
-        }else{
+        }else if (lastValue && holdOp === 'subtract'){
             lastValue = Number(lastValue) - Number(inputBox.textContent);
-            inputBox.textContent = '';
             console.log(lastValue)
         }
+        holdOp = 'subtract';
     }
 
     let clickEqual = document.querySelector('#equal');
