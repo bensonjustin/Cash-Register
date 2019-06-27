ad58c7edff05826b17701118fa7f872b8e97120f
Justin <jbbenson12@gmail.com>
2019-06-26 21:17:49 -1000

    cash reg

--- js/calculator.js
+++ js/calculator.js
@@ -3,16 +3,20 @@ let calculator = (function(){
     let holdOp = '';
     let lastValue = 0;
     let currentValue = 0;
+    let balValue = 0;
 
     let clickNum = document.querySelectorAll('.num');
     for (let i = 0; i<clickNum.length; i++){
     clickNum[i].addEventListener('click', dispNum);
     function dispNum(){
-        if (inputBox.textContent.length < 10 && inputBox.textContent !== '0' && lastValue != inputBox.textContent && currentValue != inputBox.textContent){
+        if (inputBox.textContent.length < 10 && !holdOp){
             inputBox.textContent = inputBox.textContent + document.getElementsByClassName('num')[i].innerHTML;
-        }else if (lastValue == inputBox.textContent || currentValue == inputBox.textContent){
-            inputBox.textContent = '';
+            lastValue = inputBox.textContent
+            console.log('lastValue ' + lastValue)
+        }else if (lastValue || lastValue === 0 && holdOp !== 'equal'){
             inputBox.textContent = inputBox.textContent + document.getElementsByClassName('num')[i].innerHTML;
+            currentValue = inputBox.textContent;
+            console.log('currentValue ' + currentValue)
         }
     }
     }
@@ -23,8 +27,16 @@ let calculator = (function(){
     function disp0(){
         if (inputBox.textContent.length === 0){
             inputBox.textContent = num0.innerHTML;
-        }else if (inputBox.textContent.length < 10 && inputBox.textContent !== '0'){
+            lastValue = inputBox.textContent
+            console.log('lastValue ' + lastValue)
+        }else if (inputBox.textContent.length < 10 && !holdOp){
             inputBox.textContent = inputBox.textContent + num0.innerHTML;
+            lastValue = inputBox.textContent
+            console.log('lastValue ' + lastValue)
+        }else if (lastValue){
+            inputBox.textContent = inputBox.textContent + num0.innerHTML;
+            currentValue = inputBox.textContent;
+            console.log('currentValue ' + currentValue)
         }
     }
 
@@ -34,8 +46,14 @@ let calculator = (function(){
     function disp00(){
         if (inputBox.textContent.length === 0){
 
-        }else if (inputBox.textContent.length < 9 && inputBox.textContent !== '0'){
+        }else if (inputBox.textContent.length < 9 && !holdOp){
             inputBox.textContent = inputBox.textContent + num00.innerHTML;
+            lastValue = inputBox.textContent
+            console.log('lastValue ' + lastValue)
+        }else if (lastValue){
+            inputBox.textContent = inputBox.textContent + num00.innerHTML;
+            currentValue = inputBox.textContent;
+            console.log('currentValue ' + currentValue)
         }
     }
 
@@ -50,34 +68,70 @@ let calculator = (function(){
         }
     }
 
+    function calcOp(){
+        inputBox.textContent = '';
+        if (holdOp === 'add'){
+            lastValue = Number(lastValue) + Number(currentValue)
+            console.log("lastValue " + lastValue)
+            inputBox.textContent = lastValue;
+        }else if (holdOp === 'subtract'){
+            lastValue = Number(lastValue) - Number(currentValue)
+            console.log("lastValue " + lastValue)
+            inputBox.textContent = lastValue;
+        }else if (holdOp === 'multiply'){
+            lastValue = Number(lastValue) * Number(currentValue)
+            console.log("lastValue " + lastValue)
+            inputBox.textContent = lastValue;
+        }
+        else if (holdOp === 'divide'){
+            lastValue = Number(lastValue) / Number(currentValue)
+            console.log("lastValue " + lastValue)
+            inputBox.textContent = lastValue;
+        }
+    }
+
     let clickAdd = document.querySelector('#add');
     clickAdd.addEventListener('click', dispAdd);
         
     function dispAdd(){
-        currentValue = inputBox.textContent;
-        if (!lastValue){
-            lastValue = Number(inputBox.textContent);
-            console.log('firstadd')
-        }else if (lastValue && holdOp === 'add'){
-            lastValue = Number(lastValue) + Number(inputBox.textContent);
-            console.log(lastValue)
+        calcOp();
+        if (lastValue){
+            holdOp = 'add';
+            console.log(holdOp)
         }
-        holdOp = 'add';
     }
 
     let clickSubtract = document.querySelector('#subtract');
     clickSubtract.addEventListener('click', dispSubtract);
 
     function dispSubtract(){
-        currentValue = inputBox.textContent;
-        if (!lastValue){
-            lastValue = Number(inputBox.textContent)
-            console.log('firstsubtract')
-        }else if (lastValue && holdOp === 'subtract'){
-            lastValue = Number(lastValue) - Number(inputBox.textContent);
-            console.log(lastValue)
+        calcOp();
+        if (lastValue){
+            holdOp = 'subtract';
+            console.log(holdOp)
         }
-        holdOp = 'subtract';
+    }
+
+    let clickMultiply = document.querySelector('#multiply');
+    clickMultiply.addEventListener('click', dispMultiply);
+
+    function dispMultiply(){
+        calcOp();
+        if (lastValue){
+            holdOp = 'multiply';
+            console.log(holdOp)
+        }
+    }
+
+    let clickDivide = document.querySelector('#divide');
+    clickDivide.addEventListener('click', dispDivide);
+
+    function dispDivide(){
+        calcOp();
+        if (lastValue){
+            holdOp = 'divide';
+            console.log(holdOp)
+        }
     }
 
     let clickEqual = document.querySelector('#equal');
@@ -85,10 +139,22 @@ let calculator = (function(){
 
     function dispEqual(){
         if (holdOp === 'add'){
-            inputBox.textContent = Number(lastValue) + Number(inputBox.textContent);
+            inputBox.textContent = Number(lastValue) + Number(currentValue);
             console.log(holdOp)
         }else if (holdOp === 'subtract'){
-            inputBox.textContent = Number(lastValue) - Number(inputBox.textContent);
+            inputBox.textContent = Number(lastValue) - Number(currentValue);
+            console.log(holdOp)
+        }else if (holdOp === 'multiply'){
+            inputBox.textContent = Number(lastValue) * Number(currentValue);
+            console.log(holdOp)
+        }
+        else if (holdOp === 'divide'){
+            inputBox.textContent = Number(lastValue) / Number(currentValue);
+            console.log(holdOp)
+        }
+        if (lastValue && currentValue){
+            holdOp = 'equal';
+            lastValue = inputBox.textContent;
             console.log(holdOp)
         }
     }
@@ -99,15 +165,59 @@ let calculator = (function(){
     function dispClear(){
         inputBox.textContent = '';
         lastValue = 0;
+        currentValue = 0;
+        holdOp = '';
     }
 
-// return {
-//     dispNum: dispNum,
-//     disp0: disp0,
-//     disp00: disp00,
-//     dispAdd: dispAdd,
-//     dispEqual: dispEqual,
-//     dispClear: dispClear
-// }
+    let clickGetBal = document.querySelector('#getbal');
+    clickGetBal.addEventListener('click', dispGetBal);
+
+    let getBalDisp = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      });
+
+    function dispGetBal(){
+        inputBox.textContent = getBalDisp.format(balValue);
+    }
+
+    let clickDeposit = document.querySelector('#deposit');
+    clickDeposit.addEventListener('click', dispDeposit);
+
+    function dispDeposit(){
+        balValue = Number(balValue) + Number(inputBox.textContent);
+        console.log(balValue)
+        dispClear();
+    }
+
+    let clickWithdraw = document.querySelector('#withdraw');
+    clickWithdraw.addEventListener('click', dispWithdraw);
+
+    function dispWithdraw(){
+        if (balValue < inputBox.textContent){
+            window.alert('Not enough funds');
+        }else{
+            balValue = Number(balValue) - Number(inputBox.textContent);
+            console.log(balValue)
+            dispClear();
+        }
+    }
+
+return {
+    dispNum: dispNum,
+    disp0: disp0,
+    disp00: disp00,
+    dispDecimal: dispDecimal,
+    calcOp: calcOp,
+    dispAdd: dispAdd,
+    dispSubtract: dispSubtract,
+    dispMultiply: dispMultiply,
+    dispDivide: dispDivide,
+    dispEqual: dispEqual,
+    dispClear: dispClear,
+    dispGetBal: dispGetBal,
+    dispDeposit: dispDeposit,
+    dispWithdraw: dispWithdraw
+}
 
 })();
